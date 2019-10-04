817b52ad56675c7f56151aa1a45fca247403ab6c
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-10-04 14:24:44 +0100

    Setup project

---
+++ ruagomesfreiregamesol.py
@@ -0,0 +1,20 @@
+import math
+import pickle
+import time
+
+  
+class SearchProblem:
+
+  def __init__(self, goal, model, auxheur = []):
+    ##
+    ## to implement
+    ## 
+    pass
+
+  def search(self, init, limitexp = 2000, limitdepth = 10, tickets = [math.inf,math.inf,math.inf]):
+    ##
+    ## to implement
+    ##
+    return []
+
+    
