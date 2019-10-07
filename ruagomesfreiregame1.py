85b74af171cd8161165ec0b7973021d6d1b4f3ce
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-10-07 15:09:04 +0100

    Exercise 2 done. After validatepath, solution changes to wrong one

--- ruagomesfreiregame1.py
+++ ruagomesfreiregame1.py
@@ -12,44 +12,47 @@ with open("mapasgraph.pickle", "rb") as fp:   #Unpickling
 U = AA[1]
 
 def plotpath(P,coords):   
-        img = plt.imread('maps.png')
-        plt.imshow(img)
-        colors = ['r.-','g+-','b^-']
-        I = P[0][1]
-        for agind in range(len(P[0][1])):
-                st = I[agind]-1
-                for tt in P:                        
-                        nst = tt[1][agind]-1
-                        plt.plot([coords[st][0],coords[nst][0]],[coords[st][1],coords[nst][1]],colors[agind])
-                        st = nst
-        plt.axis('off')
-        fig = plt.gcf()
-        fig.set_size_inches(1.*18.5, 1.*10.5)
-        #fig.savefig('test2png.png', dpi=100)   
-        plt.show()
+    img = plt.imread('maps.png')
+    plt.imshow(img)
+    colors = ['r.-','g+-','b^-']
+    I = P[0][1]
+    for agind in range(len(P[0][1])):
+        st = I[agind]-1
+        for tt in P:                        
+            nst = tt[1][agind]-1
+            plt.plot([coords[st][0],coords[nst][0]],[coords[st][1],coords[nst][1]],colors[agind])
+            st = nst
+    plt.axis('off')
+    fig = plt.gcf()
+    fig.set_size_inches(1.*18.5, 1.*10.5)
+    #fig.savefig('test2png.png', dpi=100)   
+    plt.show()
         
 def validatepath(oP,I,U,tickets=[25,25,25]): 
-        if not oP:
+    if not oP:
+        return False
+    P = copy.deepcopy(oP)
+    del P[0]
+    #print("Path Copy = ", P)
+    #print("Path = ", oP)
+
+    for tt in P:
+        for agind,ag in enumerate(tt[1]):
+            #print(ag)
+            st = I[agind]
+            if tickets[tt[0][agind]]==0:
+                print('no more tickets')
                 return False
-        P = copy.deepcopy(oP)
-        del P[0]
-        for tt in P:
-                for agind,ag in enumerate(tt[1]):
-                        #print(ag)
-                        st = I[agind]
-                        if tickets[tt[0][agind]]==0:
-                                print('no more tickets')
-                                return False
-                        else:
-                                tickets[tt[0][agind]] -= 1
-                                
-                                if [tt[0][agind],ag] in U[st]:
-                                        I[agind] = ag
-                                        #pass
-                                else:
-                                        print('invalid action')
-                                        return False
-        return True
+            else:
+                tickets[tt[0][agind]] -= 1
+                
+                if [tt[0][agind],ag] in U[st]:
+                    I[agind] = ag
+                    #pass
+                else:
+                    print('invalid action')
+                    return False
+    return True
 
 tinittotal = time.process_time()
 
@@ -61,14 +64,13 @@ I = [30]
 nn = SP.search(I,limitexp = 2000)
 tend = time.process_time()
 print("%.1fms"%((tend-tinit)*1000))
-print('Path is: ')
-print(nn)
+#print("Before validate: ", nn)
 if validatepath(nn,I,U):   
-        print("path")
-        print(nn)
-        plotpath(nn,coords)     
+    print("path")
+    print(nn)
+    plotpath(nn,coords)     
 else:
-        print("invalid path")
+    print("invalid path")
 
 print("\n(4 val) Exercise 2 - One agent, Limits")
 print("Init [30] Goal [56]")
@@ -78,13 +80,14 @@ I = [30]
 nn = SP.search(I,limitexp = 2000, tickets = [5,5,2])
 tend = time.process_time()
 print("%.1fms"%((tend-tinit)*1000))
+#print("Before validate: ", nn)
 if validatepath(nn,I,U, tickets = [5,5,2]):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
+    print("path")
+    print(nn)
+    plotpath(nn,coords)
 else:
-        print("invalid path")
-
+    print("invalid path")
+"""
 print("\n(6 val) Exercise 3 - Three agents, No limits (test 1)")
 print("Init [1,3,7] Goal [2,21,9]")
 SP = SearchProblem(goal = [2,21,9], model = U, auxheur=coords)
@@ -94,11 +97,11 @@ nn = SP.search(I,limitexp = 2000)
 tend = time.process_time()
 print("%.1fms"%((tend-tinit)*1000))
 if validatepath(nn,I,U):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
+    print("path")
+    print(nn)
+    plotpath(nn,coords)
 else:
-        print("invalid path")
+    print("invalid path")
 
 print("\n(6 val) Exercise 3 - Three agents, No limits (test 2)")
 print("Init [30,40,109] Goal [61,60,71]")
@@ -109,11 +112,11 @@ nn = SP.search(I,limitexp = 2000)
 tend = time.process_time()
 print("%.1fms"%((tend-tinit)*1000))
 if validatepath(nn,I,U):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
+    print("path")
+    print(nn)
+    plotpath(nn,coords)
 else:
-        print("invalid path")
+    print("invalid path")
         
 print("\n(4 val) Exercise 4 - Three agents, Limits")
 print("Init [30,40,109] Goal [61,60,71]")
@@ -124,15 +127,15 @@ nn = SP.search(I,limitexp = 3000, limitdepth = 10, tickets = [5,20,2])
 tend = time.process_time()
 print("%.1fms"%((tend-tinit)*1000))
 if validatepath(nn,I,U, tickets = [5,20,2]):
-        print("path")
-        print(nn)
-        plotpath(nn,coords)
+    print("path")
+    print(nn)
+    plotpath(nn,coords)
 else:
-        print("invalid path")
+    print("invalid path")
 
 print("\n(4 val) Exercise 5 - Three agents, Limits, Any-Order")
 
 tendtotal = time.process_time()
 print("Total time %.1fms"%((tendtotal-tinittotal)*1000))
 
-
+"""
--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -20,16 +20,29 @@ class SearchProblem:
     def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
         bound = self.h(init[0])
         # user init como path
-        while True:
-            t = self.find(init, 0, bound)
-            if t == 'FOUND':
-                return init
-            elif t == math.inf:
-                return []
-            bound = t
+        path = [[[], init]]         # path = [[[transport], [node]], [...]]
+        n_detectives = len(init)
+        if (n_detectives == 1 and tickets[0] == math.inf):
+            while True:
+                t = self.find_1(path, 0, bound)
+                if t == 'FOUND':
+                    return path
+                elif t == math.inf:
+                    return []
+                bound = t
 
-    def find(self, path, g, bound):
-        node = path[-1]
+        elif (n_detectives == 1 and tickets[0] != math.inf):
+            while True:
+                t = self.find_2(path, 0, bound, tickets)
+                if t == 'FOUND':
+                    return path
+                elif t == math.inf:
+                    return []
+                bound = t
+
+
+    def find_1(self, path, g, bound):
+        node = path[-1][-1][0]
         f = g + self.h(node)
         if f > bound:
             return f
@@ -38,11 +51,37 @@ class SearchProblem:
         min = math.inf
         for succ in self.model[node]:
             if succ not in path:
-                path.append(succ[1])
-                t = self.find(path, f + self.cost(node, succ[1]), bound)
+                proper_succ = [[succ[0]], [succ[1]]]
+                path.append(proper_succ)
+                t = self.find_1(path, f + self.cost(node, succ[1]), bound)
                 if t == 'FOUND':
                     return 'FOUND'
                 if t < min:
                     min = t
                 path.pop()
+                
+        return min
+
+
+    def find_2(self, path, g, bound, tickets):
+        node = path[-1][-1][0]
+        f = g + self.h(node)
+        if f > bound:
+            return f
+        if node in self.goal:
+            return 'FOUND'
+        min = math.inf
+        for succ in self.model[node]:
+            if succ not in path and tickets[succ[0]] > 0:
+                proper_succ = [[succ[0]], [succ[1]]]
+                path.append(proper_succ)
+                tickets[succ[0]] -= 1
+                t = self.find_2(path, f + self.cost(node, succ[1]), bound, tickets)
+                if t == 'FOUND':
+                    return 'FOUND'
+                if t < min:
+                    min = t
+                path.pop()
+                tickets[succ[0]] += 1
+
         return min
