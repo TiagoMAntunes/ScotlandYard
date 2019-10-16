d2147a0b6a92ccf9c9399c4a23a0be75e657b740
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-10-16 11:22:14 +0100

    Updated cost and heuristics.

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -9,13 +9,15 @@ class SearchProblem:
         self.goal = goal
         self.model = model
         self.coords = auxheur
+        self.max_dist = math.sqrt((self.coords[31][0] - self.coords[112][0])**2 + (self.coords[31][1] - self.coords[112][1])**2)
 
     def cost(self, node, succ):
-        return math.sqrt((self.coords[node][0] - self.coords[succ][0])**2 + (self.coords[node][1] - self.coords[succ][1])**2)
+        dist = math.sqrt((self.coords[node][0] - self.coords[succ][0])**2 + (self.coords[node][1] - self.coords[succ][1])**2)
+        return dist / self.max_dist
 
     def h(self, start_node, goal_index=0):
-        """ Heuristica: distancia no mapa """
-        return self.cost(start_node, self.goal[goal_index])
+        """ Heuristica: distancia no mapa / max dist entre 2 pontos """
+        return self.cost(start_node, self.goal[goal_index])      
 
     def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
         bound = self.h(init[0])
@@ -24,16 +26,16 @@ class SearchProblem:
         n_detectives = len(init)
         if (n_detectives == 1 and tickets[0] == math.inf):
             while True:
-                t = self.find_1(path, 0, bound)
+                t = self.find_1(path, 1, bound)
                 if t == 'FOUND':
                     return path
                 elif t == math.inf:
                     return []
                 bound = t
 
-        elif (n_detectives == 1 and tickets[0] != math.inf):
+        elif (n_detectives == 1 and tickets[1] != math.inf):
             while True:
-                t = self.find_2(path, 0, bound, tickets)
+                t = self.find_2(path, 1, bound, tickets)
                 if t == 'FOUND':
                     return path
                 elif t == math.inf:
@@ -48,6 +50,7 @@ class SearchProblem:
             return f
         if node in self.goal:
             return 'FOUND'
+
         min = math.inf
         for succ in self.model[node]:
             if succ not in path:
@@ -62,7 +65,6 @@ class SearchProblem:
                 
         return min
 
-
     def find_2(self, path, g, bound, tickets):
         node = path[-1][-1][0]
         f = g + self.h(node)
@@ -70,6 +72,7 @@ class SearchProblem:
             return f
         if node in self.goal:
             return 'FOUND'
+            
         min = math.inf
         for succ in self.model[node]:
             if succ not in path and tickets[succ[0]] > 0:
@@ -85,3 +88,4 @@ class SearchProblem:
                 tickets[succ[0]] += 1
 
         return min
+
