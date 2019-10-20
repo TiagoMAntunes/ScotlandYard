9257b9f7264d7d00a1951d26867cf5cafb092210
TiagoMAntunes <tiago.melo.antunes@tecnico.ulisboa.pt>
2019-10-20 12:38:09 +0100

    Calculates shortest path for each agent

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -1,7 +1,7 @@
 import math
 import pickle
 import time
-
+from pprint import pprint as p
 
 class SearchProblem:
 
@@ -20,57 +20,32 @@ class SearchProblem:
         return self.cost(start_node, self.goal[goal_index])      
 
     def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
-        bound = self.h(init[0])
-        # user init como path
+        n_detectives = range(len(self.goal))
+        shortest_paths = [0] * len(n_detectives)
+        for i in n_detectives:
+            self.current_goal = self.goal[i]
+            print(self.current_goal)
+            shortest_paths[i] = self.IDA([init[i]], limitexp, limitdepth, tickets)
+        p(shortest_paths)
+
+    def IDA(self, init, limitexp, limitdepth, tickets):
+        bound = self.h(init[0]) #initial limit
         path = [[[], init]]         # path = [[[transport], [node]], [...]]
-        n_detectives = len(init)
-        if (n_detectives == 1 and tickets[0] == math.inf):
-            while True:
-                t = self.find_1(path, 1, bound)
-                if t == 'FOUND':
-                    return path
-                elif t == math.inf:
-                    return []
-                bound = t
+        while True:
+            t = self.find(path, 1, bound, tickets)
+            if t == 'FOUND':
+                return path
+            elif t == math.inf:
+                return []
+            bound = t
+        return path
 
-        elif (n_detectives == 1 and tickets[1] != math.inf):
-            while True:
-                t = self.find_2(path, 1, bound, tickets)
-                if t == 'FOUND':
-                    return path
-                elif t == math.inf:
-                    return []
-                bound = t
-
-
-    def find_1(self, path, g, bound):
+    def find(self, path, g, bound, tickets = [math.inf, math.inf, math.inf]):
         node = path[-1][-1][0]
         f = g + self.h(node)
         if f > bound:
             return f
-        if node in self.goal:
-            return 'FOUND'
-
-        min = math.inf
-        for succ in self.model[node]:
-            if succ not in path:
-                proper_succ = [[succ[0]], [succ[1]]]
-                path.append(proper_succ)
-                t = self.find_1(path, f + self.cost(node, succ[1]), bound)
-                if t == 'FOUND':
-                    return 'FOUND'
-                if t < min:
-                    min = t
-                path.pop()
-                
-        return min
-
-    def find_2(self, path, g, bound, tickets):
-        node = path[-1][-1][0]
-        f = g + self.h(node)
-        if f > bound:
-            return f
-        if node in self.goal:
+        if node == self.current_goal:
             return 'FOUND'
             
         min = math.inf
@@ -79,7 +54,7 @@ class SearchProblem:
                 proper_succ = [[succ[0]], [succ[1]]]
                 path.append(proper_succ)
                 tickets[succ[0]] -= 1
-                t = self.find_2(path, f + self.cost(node, succ[1]), bound, tickets)
+                t = self.find(path, f + self.cost(node, succ[1]), bound, tickets)
                 if t == 'FOUND':
                     return 'FOUND'
                 if t < min:
