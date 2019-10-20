6391655e3b5f2d4acd7b51797a2e504798e4b5c2
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-10-20 20:45:36 +0100

    Gets collisions. Recalculates paths avoiding conflicts.

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -11,6 +11,7 @@ class SearchProblem:
         self.coords = auxheur
         self.max_dist = math.sqrt((self.coords[31][0] - self.coords[112][0])**2 + (self.coords[31][1] - self.coords[112][1])**2)
         self.busy_times = {}
+        self.collisions = [{}, {}, {}]	# [{t0: [[dx, dy], node], t1: ...}, {...}, {...}]
 
     def cost(self, node, succ):
         dist = math.sqrt((self.coords[node-1][0] - self.coords[succ-1][0])**2 + (self.coords[node-1][1] - self.coords[succ-1][1])**2)
@@ -18,7 +19,8 @@ class SearchProblem:
 
     def h(self, start_node, goal_index=0):
         """ Heuristica: distancia no mapa / max dist entre 2 pontos """
-        return self.cost(start_node, self.goal[goal_index])      
+        return self.cost(start_node, self.goal[goal_index])  
+
 
     def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
         def add_positions(list):
@@ -28,13 +30,45 @@ class SearchProblem:
                 else:
                     self.busy_times[i].append(list[i][1][0])
 
+        def get_collisions(n_detectives, paths):
+            for i in range(len(n_detectives) -1):
+                for j in range(path_lengths[i]):
+                    node = shortest_paths[i][j][1][0]
+                    if (j <= path_lengths[i+1] and node == shortest_paths[i+1][j][1][0]):
+                        if j not in self.collisions[i]:
+                            self.collisions[i+1][j] = [[i], node]
+                        else:
+                            self.collisions[i+1][j][0].append(i)
+
         n_detectives = range(len(self.goal))
         shortest_paths = [0] * len(n_detectives)
         for i in n_detectives:
             self.current_goal = self.goal[i]
-            shortest_paths[i] = self.IDA([init[i]], limitexp, limitdepth, tickets)
+            shortest_paths[i] = self.IDA([init[i]], limitexp, limitdepth, tickets, i)
             print("shortest", i, " ", shortest_paths[i])
-        
+
+        # calculate each path's length
+        path_lengths = []
+        for path in shortest_paths:
+        	path_lengths.append(len(path))
+
+        # get collisions for each path
+        get_collisions(n_detectives, shortest_paths)
+        print("collisions = ", self.collisions)
+
+        # recalculate paths with collisions, avoiding them
+        uncolliding_paths = [0] * len(n_detectives)
+        for i in n_detectives:
+            if self.collisions[i]:
+                uncolliding_paths[i] = self.IDA([init[i]], limitexp, limitdepth, tickets, i)
+
+            else:
+                uncolliding_paths[i] = shortest_paths[i]
+
+            add_positions(uncolliding_paths[i])
+
+        print("not colliding = ", uncolliding_paths)
+
         #find the limiting path, the one that is the longest between the shortest paths
         longest_index = 0
         for i, path in enumerate(shortest_paths[1:]):
@@ -42,8 +76,7 @@ class SearchProblem:
                 longest_index = i
 
         #longest_index holds the index of the agent with the longest path
-        add_positions(shortest_paths[longest_index])
-        print("longest index = ", longest_index)
+        # add_positions(shortest_paths[longest_index])
         for i in range(len(shortest_paths)):
             if (i == longest_index):
                 continue
@@ -52,6 +85,7 @@ class SearchProblem:
             print('Current busy positions: ' + str(self.busy_times))
             shortest_paths[i] = self.IDFS(shortest_paths[longest_index], init[i], limitexp, limitdepth, tickets)
             add_positions(shortest_paths[i])
+
 
         res = []
 
@@ -65,11 +99,11 @@ class SearchProblem:
 
         
 
-    def IDA(self, init, limitexp, limitdepth, tickets):
+    def IDA(self, init, limitexp, limitdepth, tickets, detective):
         bound = self.h(init[0]) 	#initial limit
         path = [[[], init]]         # path = [[[transport], [node]], [...]]
         while True:
-            t = self.find(path, 1, bound, tickets)
+            t = self.find(path, 1, bound, 0, detective, tickets)
             if t == 'FOUND':
                 return path
             elif t == math.inf:
@@ -77,7 +111,7 @@ class SearchProblem:
             bound = t
         return path
 
-    def find(self, path, g, bound, tickets = [math.inf, math.inf, math.inf]):
+    def find(self, path, g, bound, time, detective, tickets = [math.inf, math.inf, math.inf]):
         node = path[-1][-1][0]
         f = g + self.h(node)
         if f > bound:
@@ -88,11 +122,20 @@ class SearchProblem:
         min = math.inf
 
         for succ in self.model[node]:
-            if succ not in path and tickets[succ[0]] > 0:
+            # detective cant go to unreachable places, nor use transport without tickets
+            # detective cant go to places where it has collisions
+            # detective cant create new collisions
+            if succ not in path and tickets[succ[0]] > 0 and \
+            (not self.collisions[detective] or \
+            time+1 not in self.collisions[detective] or \
+            self.collisions[detective][time+1][1] != succ[1]) and \
+            (time+1 not in self.busy_times or \
+            succ[1] not in self.busy_times[time+1]):
+
                 proper_succ = [[succ[0]], [succ[1]]]
                 path.append(proper_succ)
                 tickets[succ[0]] -= 1
-                t = self.find(path, f + self.cost(node, succ[1]), bound, tickets)
+                t = self.find(path, f + self.cost(node, succ[1]), bound, time+1, detective, tickets)
                 if t == 'FOUND':
                     return 'FOUND'
                 if t < min:
@@ -112,11 +155,9 @@ class SearchProblem:
             elif depth > 0:
                 any_remaining = False
                 for child in self.model[node]:
-                    print("child of ", node, "is ", child)
                     if child[1] in self.busy_times[time+1]: #busy position at the time
                         continue
                     found, remaining = DLS(depth - 1, child[1], time + 1)
-                    print("found = ", found)
                     if found != None:
                         proper_succ = [[child[0]], [child[1]]]
                         return [proper_succ] + found, True
@@ -124,5 +165,5 @@ class SearchProblem:
                         any_remaining = True
                 return None, any_remaining
 
-        return [[[], [start_node]]] + DLS(len(longest_path) - 1, start_node, 0)[0]
+        return [[[], [start_node]]] + DLS(len(longest_path)-1, start_node, 0)[0]
 
