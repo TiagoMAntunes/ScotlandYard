febc10f3bb09e7b9d40e722d740deb8e4ffaa8f1
Tiago Antunes <tiago.melo.antunes@gmail.com>
2019-10-21 11:17:09 +0100

    BFS with tickets. Get longest path

--- ruagomesfreiregamesol.py
+++ ruagomesfreiregamesol.py
@@ -12,7 +12,6 @@ class SearchProblem:
         self.coords = auxheur
 
     def format(self, parent, child):
-        
         return [[[self.model[parent][child][0]], [self.model[parent][child][1]]]]
 
     def search(self, init, limitexp=2000, limitdepth=10, tickets=[math.inf, math.inf, math.inf]):
@@ -27,32 +26,50 @@ class SearchProblem:
         #BFS on all agents
         paths = []
         for i in range(len(self.goal)):
+            self.tickets = tickets
             self.BFS(init[i], self.goal[i])
             paths.append(backtrack(self.goal[i]))
+            print(self.parents)
+            
         #Validate longest path
-
-        p(paths)
-
+        longest = 0
+        for i, path in enumerate(paths[1:]):
+            if len(paths[longest]) < len(path):
+                longest = i
+        
         #SDFS for all paths with specific size adapting to longer sizes
 
 
     def BFS(self, node, goal):
-        self.parents = [[-math.inf, 0] * x for x in range(len(self.model) + 1)] #create parents array
+        self.parents = [[-math.inf, 0, [0,0,0]] for x in range(len(self.model) + 1)] #create parents array
         self.distances = [-math.inf] * (len(self.model)+1) #create distances array
         self.visited = [False] * (len(self.model)+1)
         Q = queue.Queue()
         Q.put(node)
         self.distances[node] = 0
         self.visited[node] = True
+        self.parents[node][2] = self.tickets
+        
         while not Q.empty():
             u = Q.get()
             i = 0
+            print('Node: ' + str(u))
             for transition in self.model[u]: #check possible paths
+                #Validate child
                 child = transition[1]
+                if self.parents[u][2][transition[0]] <= 0:
+                    continue
+        
                 if not self.visited[child]: #not visited
                     self.visited[child] = True
                     self.distances[child] = self.distances[u] + 1
-                    self.parents[child] = [u, i]
+                    
+                    #update tickets
+                    tickets = list(self.parents[u][2])
+                    tickets[transition[0]] -= 1
+                    self.parents[child] = [u, i, tickets]
+                    
+                    #stop earlier
                     if child == goal:
                         return child
                     Q.put(child)
